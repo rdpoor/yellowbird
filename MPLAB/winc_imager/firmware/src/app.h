868ab0cd0c15a7135916397926518e70d0aa2b73
R. D. Poor <rdpoor@gmail.com>
2021-12-24 11:40:49 -0800

    add WIP winc_imager

---
+++ MPLAB/winc_imager/firmware/src/app.h
@@ -0,0 +1,34 @@
+/**
+ * @file: template.h
+ */
+
+#ifndef _APP_H_
+#define _APP_H_
+
+// *****************************************************************************
+// Includes
+
+#include <stdbool.h>
+#include <stdint.h>
+
+// =============================================================================
+// C++ compatibility
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// *****************************************************************************
+// Public types and definitions
+
+void APP_Initialize(void);
+void APP_Tasks(void);
+
+// *****************************************************************************
+// Public declarations
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* #ifndef _APP_H_ */
